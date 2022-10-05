fd495ef232e116d5a009efa66ea3a04db99504b6
slayoffer <slayoffer@gmail.com>
2022-10-05 15:53:26 +1000

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-// import { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  NavLink,
-} from 'react-router-dom';
-// import Card from './components/shared/Card';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-// import FeedbackItem from './components/FeedbackItem';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-// import FeedbackData from './data/FeedbackData';
+import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 import { FeedbackProvider } from './context/FeedbackContext';
-import AboutIconLink from './components/AboutIconLink';
-// import Post from './components/Post';
 
 function App() {
-  // const [feedback, setFeedback] = useState(FeedbackData);
-
   return (
     <FeedbackProvider>
       <Router>
-        {/* <Header bgColor="red" textColor="blue" /> */}
         <Header />
         <div className="container">
           <Routes>
             <Route
-              exact
               path="/"
               element={
                 <>
@@ -38,57 +24,15 @@ function App() {
                 </>
               }
             />
+
             <Route path="/about" element={<AboutPage />} />
-            {/* <Route path="/post/:id/:name" element={<Post />} /> */}
-            {/* <Route path=" /post/*" element={<Post />} /> */}
           </Routes>
+
+          <AboutIconLink />
         </div>
-        {/* <Card>
-        <NavLink to="/" activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink to="/about" activeClassName="active">
-          About
-        </NavLink>
-      </Card> */}
-        <AboutIconLink />
       </Router>
     </FeedbackProvider>
   );
-
-  // const title = 'Blog post';
-  // const body = 'This is my blog post';
-  // const comments = [
-  //   { id: 1, text: 'Comment One' },
-  //   { id: 2, text: 'Comment Two' },
-  //   { id: 3, text: 'Comment Three' },
-  // ];
-
-  // const loading = false;
-  // const showComments = true;
-
-  // if (loading) {
-  //   return <h1>Loading...</h1>;
-  // }
-
-  // const commentBlock = (
-  //   <div className="comments">
-  //     <h3>Comments ({comments.length})</h3>
-  //     <ul>
-  //       {comments.map((comment, index) => (
-  //         <li key={index}>{comment.text}</li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
-
-  // return (
-  //   <div className="container">
-  //     <h1>{title.toLowerCase()}</h1>
-  //     <p>{body}</p>
-  //     {showComments && commentBlock}
-  //   </div>
-  // );
 }
 
 export default App;
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,10 @@
 import { useState, useContext, useEffect } from 'react';
-
-// import PropTypes from 'prop-types';
+import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
-import RatingSelect from './RatingSelect';
 import FeedbackContext from '../context/FeedbackContext';
 
-export default function FeedbackForm() {
+function FeedbackForm() {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -23,20 +21,24 @@ export default function FeedbackForm() {
     }
   }, [feedbackEdit]);
 
-  const handleTextChange = (event) => {
-    if (text === '') {
-      setBtnDisabled(true);
-      setMessage(null);
-    } else if (text !== '' && text.trim().length <= 10) {
-      setMessage('Text mush be at least 10 characters long');
-      setBtnDisabled(true);
-    } else {
-      setMessage(null);
-      setBtnDisabled(false);
-    }
+  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
 
-    setText(event.target.value);
-  };
+  // prettier-ignore
+  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      
+  // prettier-ignore
+    } else if (value.trim().length < 10) { // 👈 check for less than 10
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(value)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -45,22 +47,26 @@ export default function FeedbackForm() {
         text,
         rating,
       };
+
       if (feedbackEdit.edit === true) {
         updateFeedback(feedbackEdit.item.id, newFeedback);
       } else {
         addFeedback(newFeedback);
       }
 
+      // NOTE: reset to default state after submission
+      setBtnDisabled(true); // 👈  add this line to reset disabled
+      setRating(10); // 👈 add this line to set rating back to 10
       setText('');
     }
   };
 
+  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        {/* eslint-disable-next-line no-shadow */}
-        <RatingSelect select={(rating) => setRating(rating)} />
+        <RatingSelect select={setRating} selected={rating} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -72,12 +78,11 @@ export default function FeedbackForm() {
             Send
           </Button>
         </div>
+
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
 }
 
-// FeedbackForm.propTypes = {
-//   handleAdd: PropTypes.func,
-// };
+export default FeedbackForm;
